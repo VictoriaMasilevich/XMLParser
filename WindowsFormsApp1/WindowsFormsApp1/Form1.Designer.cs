d2b3fe2d5cd0eaf185a7d8ac46b2d4c4421c3a58
Victoria Masilevich <tori_masya@mail.ru>
2018-05-29 14:37:03 +0300

    first commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Location = new System.Drawing.Point(57, 144);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(160, 72);
+            this.buttonGo.TabIndex = 0;
+            this.buttonGo.Tag = "Изменить цвет";
+            this.buttonGo.Text = "Parse";
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(57, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "C:\\Users\\Admin\\Desktop\\ооп 6\\feed-test.xml";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(57, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(160, 69);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GreenYellow;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.buttonGo);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Form1";
+            this.Text = "Parser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox textBox2;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGo_Click(object sender, EventArgs e)
+        {
+            string KeyValue;
+            string FilePath = textBox1.Text.ToString();
+            try
+            {
+                using (XmlTextReader XmlReader = new XmlTextReader(FilePath))
+                {
+                    Dictionary<string, int> TagDictionary = new Dictionary<string, int>();
+                    TagDictionary.Add("no-role", 0);
+                    while (XmlReader.Read())
+                    {
+                        if (XmlReader.NodeType == XmlNodeType.Element)
+                        {
+                            if (XmlReader.Name == "user" && (XmlReader.GetAttribute("role")!=null))
+                            {
+                                KeyValue = XmlReader.GetAttribute("role");
+                                if (KeyValue != null)
+                                {
+                                    if (!TagDictionary.ContainsKey(KeyValue))
+                                    {
+                                        TagDictionary.Add(KeyValue, 1);
+                                    }
+                                    else
+                                    {
+                                        TagDictionary[KeyValue] = ++TagDictionary[KeyValue];
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                if (XmlReader.Name == "user")
+                                {
+                                    TagDictionary["no-role"] = ++TagDictionary["no-role"];
+                                }
+                            }
+                        }
+
+                    }
+                    foreach (KeyValuePair<string, int> output in TagDictionary)
+                    {
+                        textBox2.Items.Add(output.Key + ": " + output.Value);
+                    }
+
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("Файл не найден!");
+            }
+            catch (XmlException)
+            {
+                MessageBox.Show("Ошибка считывания!");
+            }
+            catch
+            {
+                MessageBox.Show("Неверный путь!");
+            }
+        }
+    }
+}
